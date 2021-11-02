25d09802ebff4d7a6e395941c6c30e7d6abb24e6
Nuccia955 <elenamunaro1995@gmail.com>
2021-11-02 16:58:51 +0100

    add background onclicked squares+clean

--- js/main.js
+++ js/main.js
@@ -1,39 +1,47 @@
-//1. switch grid dimensions with level-select and generate grid with btn-Play
+//1. DOM selection
 let mainContent = document.querySelector('.main-section');
 const btnPlay = document.querySelector('#btn-play');
 const selection = document.querySelector('#selection');
 
+//2.1. switch grid dimensions with level-select...
 btnPlay.addEventListener('click', function() {
     mainContent.innerHTML = '';
     const levelSelection = selection.value;
-    console.log(levelSelection);
     let gridNumberOfSquares;
     let gridRowCells;
     switch (levelSelection) {
-    case '1': gridNumberOfSquares = 49;
-              gridRowCells = 7;
+    case '1': gridNumberOfSquares = 100;
+              gridRowCells = 10;
               break;
     case '2': gridNumberOfSquares = 81;
               gridRowCells = 9;
               break;
-    case '3': gridNumberOfSquares = 100;
-              gridRowCells = 10;
+    case '3': gridNumberOfSquares = 49;
+              gridRowCells = 7;
     }
-    console.log(gridNumberOfSquares);
-    console.log(gridRowCells);
+
+    //2.2 ...and generate grid with btn-Play
     const grid = document.createElement('div');
     grid.classList.add('grid-wrap');
-    console.log(grid);
 
     for (let i = 1; i <= gridNumberOfSquares; i++) {
+        //4. call function to create a square
         const square = addSquare(i, gridRowCells);
         grid.append(square);
+        //5. on squares add click event for changing background-color
+        square.addEventListener('click', () => {
+            square.classList.add('bg-blue');
+        })
     }
 
     mainContent.append(grid);
 });
 
-//2. add function to create squares
+
+/************
+ * FUNCTIONS
+ * **********/
+//3. define function to create a square
 function addSquare(number, cells) {
     const square = document.createElement('div');
     square.classList.add('square');
@@ -42,9 +50,7 @@ function addSquare(number, cells) {
     const squareNumber = document.createElement('span');
     squareNumber.append(`${number}`);
     square.append(squareNumber);
-    console.log(square);
     return square;
 }
 
 
-//3. on squares add click event for changing background-color
